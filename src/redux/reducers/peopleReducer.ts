7538cada244405482672fe8e576c94296bfa947e
Gess <newerr161@gmail.com>
2022-01-17 16:40:06 +0200

    added set people reducer

--- src/redux/reducers/peopleReducer.ts
+++ src/redux/reducers/peopleReducer.ts
@@ -1,17 +1,21 @@
 import { AnyAction } from "redux";
-import { FETCH_PEOPLE_FAILURE, FETCH_PEOPLE_SUCCES, FETCH_PEOPLE_REQUEST } from "../../constants";
+import { FETCH_PEOPLE_FAILURE, FETCH_PEOPLE_SUCCES, FETCH_PEOPLE_REQUEST, SET_PERSON } from "../../constants";
 import { PeopleState } from "../../types"
 
 const initialState: PeopleState = {
     pending: false,
     error: null,
     people: [],
-    person: {},
     next: ""
 }
 
 export default function peopleReducer(state = initialState, action: AnyAction): PeopleState {
     switch (action.type) {
+        case SET_PERSON:
+            return {
+                ...state,
+                person: action.payload
+            }
         case FETCH_PEOPLE_REQUEST:
             return {
                 ...state,
