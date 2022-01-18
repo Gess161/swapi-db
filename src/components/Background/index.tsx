476b26faadeeab0a92fc910c43a8cf57ad27c97f
Gess <newerr161@gmail.com>
2022-01-18 16:45:05 +0200

    added animated bg

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,10 @@
+import { Background, Stars, Twinkle } from "./styled";
+
+export default function StarrySky() {
+    return (
+        <Background>
+            <Stars />
+            <Twinkle />
+        </Background>
+    )
+}
