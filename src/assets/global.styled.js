69c72755bd4249f2476f97839e8c0b35f20548a4
Gess <newerr161@gmail.com>
2022-01-19 17:26:29 +0200

    added global styles

--- src/assets/global.styled.js
+++ src/assets/global.styled.js
@@ -5,21 +5,41 @@ const GlobalStyles = createGlobalStyle`
         box-sizing: border-box;
         padding: 0;
         margin: 0;
-        font-family: 'Montserrat', sans-serif;
     }
 
     li {
         list-style-type: none;
         white-space:nowrap;
     }
+    .block {
+        width: 100%;
+        height: 100%;
+        display: block;
+        padding-left: 5px;
+    }
     
+    .logo {
+        margin: 20px auto;
+        padding: 30px;
+        width: 45%;
+        display: flex;
+        &-container {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+        }
+    }
+
     body {
         margin: 0 auto;
         width: 100%;
         height: 100vh;
-        font-family: 'Roboto', sans-serif;
+        font-family: 'Montserrat', sans-serif;
         background-color: black;
     }
+
+    // background animation
     @keyframes move-twink-back {
         from {background-position:0 0;}
         to {background-position:-10000px 5000px;}
@@ -32,6 +52,7 @@ const GlobalStyles = createGlobalStyle`
         from {background-position:0 0;}
         to {background-position:-10000px 5000px;}
     }
+
 }
 `
 
