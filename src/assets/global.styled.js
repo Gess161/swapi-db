b13780a6d11decdea359e1d5a5472d250c6ba38a
Gess161 <newerr161@gmail.com>
2022-01-21 15:43:52 +0200

    modified styles

--- src/assets/global.styled.js
+++ src/assets/global.styled.js
@@ -29,7 +29,7 @@ const GlobalStyles = createGlobalStyle`
     .block {
         width: 100%;
         height: 100%;
-        display: block;
+        display: flex;
         padding-left: 5px;
     }
     
@@ -74,7 +74,7 @@ const GlobalStyles = createGlobalStyle`
         from {background-position:0 0;}
         to {background-position:-10000px 5000px;}
     }
-}
+
 `
 
 export default GlobalStyles;
